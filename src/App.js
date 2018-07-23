a3a5bf441da324b5d1fa8fff2880f57212749141
Nuttapon Tonout <zerozodix@gmail.com>
2018-07-23 21:36:34 +0700

    Initial app

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import firebase from 'firebase';
+
+class App extends Component {
+  constructor(props) {
+    super(props); 
+    var config = {
+      apiKey: "AIzaSyDbB0zL8PTvqb7eLTqDTltSyNKFvUDhQ_Q",
+      authDomain: "the-weding-a-ken.firebaseapp.com",
+      databaseURL: "https://the-weding-a-ken.firebaseio.com",
+      projectId: "the-weding-a-ken",
+      storageBucket: "the-weding-a-ken.appspot.com",
+      messagingSenderId: "338413489020"
+    };
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
