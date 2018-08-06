ce04f8f26413572e92cffd8dbecc35b8e4ae1414
Nuttapon Tonout <zerozodix@gmail.com>
2018-08-07 01:09:54 +0700

    try (function) : Try to add firebase Functions
    - Test function

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,9 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const functions = require("firebase-functions");
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+const admin = require('firebase-admin');
+admin.initializeApp();
+exports.addMessage = functions.region('asia-northeast1').https.onRequest((req, res) => {
+    // Grab the text parameter.
+    const original = req.query.text;
+    // Push the new message into the Realtime Database using the Firebase Admin SDK.
+    // return admin.database().ref('/messages').push({ original: original }).then((snapshot) => {
+    //     // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    //     return res.redirect(303, snapshot.ref.toString());
+    // });
+    return "test addMessage";
+});
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,3 +6,16 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.addMessage = functions.region('asia-northeast1').https.onRequest((req, res) => {
+    // Grab the text parameter.
+    const original = req.query.text;
+    // Push the new message into the Realtime Database using the Firebase Admin SDK.
+    // return admin.database().ref('/messages').push({ original: original }).then((snapshot) => {
+    //     // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    //     return res.redirect(303, snapshot.ref.toString());
+    // });
+    return "test addMessage";
+});
